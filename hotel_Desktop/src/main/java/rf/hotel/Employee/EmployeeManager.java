d8162225b7ecbdf3162b254fbe9f08b2776a9996
Rayan Faiz <121762880+Rayan-Faiz@users.noreply.github.com>
2024-01-08 23:09:42 +0100

    Add files via upload

---
+++ hotel_Desktop/src/main/java/rf/hotel/Employee/EmployeeManager.java
@@ -0,0 +1,20 @@
+package rf.hotel.Employee;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import rf.hotel.Main;
+
+import java.io.IOException;
+
+public class EmployeeManager {
+    public static void EmployeeManagementUI(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Employee-View.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
+        stage.setTitle("Employee Manager");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
